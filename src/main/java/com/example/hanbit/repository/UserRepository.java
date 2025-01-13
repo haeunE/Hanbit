cb52a2376c9a63f831536ecdcda7f5bbe3b8adc4
haeun <grace10043004@gmail.com>
2025-01-13 11:01:13 +0900

    UPDATE: admin 계정 생성, properties 적용시켜야 함

--- src/main/java/com/example/hanbit/repository/UserRepository.java
+++ src/main/java/com/example/hanbit/repository/UserRepository.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.hanbit.domain.RoleType;
@@ -12,7 +14,8 @@ import com.example.hanbit.domain.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-	
+	@Query("SELECT u FROM User u WHERE u.roleType = :roleType")
+    User findByRoleType(@Param("roleType") RoleType roleType);
 	Optional<User> findByUsername(String username);
 	Optional<User> findByEmail(String email);
 	Optional<User> findByTel(String tel);
