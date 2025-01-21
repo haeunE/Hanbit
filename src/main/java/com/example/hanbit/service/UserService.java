59b2291d987f7ed2a0cc5b58f868245c12034b1c
haeun <grace10043004@gmail.com>
2025-01-21 12:22:12 +0900

    FIX: 유효성 검사 메시지 출력 오류 해결

--- src/main/java/com/example/hanbit/service/UserService.java
+++ src/main/java/com/example/hanbit/service/UserService.java
@@ -31,25 +31,32 @@ public class UserService {
 	}
 
 	
-	public void checkUser(User user) {
-        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
-            throw new IllegalArgumentException("이미 존재하는 아이디입니다.");
-        }
-        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
-            throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
-        }
-        if (userRepository.findByTel(user.getTel()).isPresent()) {
-            throw new IllegalArgumentException("이미 존재하는 전화번호입니다.");
-        }
-    }
-	
+	public void checkUser(SignUpForm user) {
+	    // 중복된 아이디가 있는지 확인
+	    if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+	        throw new IllegalArgumentException("이미 존재하는 아이디입니다.");
+	    }
+	    // 중복된 이메일이 있는지 확인
+	    if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+	        throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
+	    }
+	    // 중복된 전화번호가 있는지 확인
+	    if (userRepository.findByTel(user.getTel()).isPresent()) {
+	        throw new IllegalArgumentException("이미 존재하는 전화번호입니다.");
+	    }
+	}
+
 	public void saveUser(SignUpForm signUpForm) {
-		User user = new User();
-		BeanUtils.copyProperties(signUpForm, user);
-	    user.setPassword(passwordEncoder.encode(signUpForm.getPassword())); // 비밀번호 암호화
+		
+	    // 사용자 존재 여부 체크
+	    checkUser(signUpForm);
+	    System.out.println("SignUpForm email: " + signUpForm.getEmail());
+	    // 새 User 객체 생성 및 필드 복사
+	    User user = new User();
+	    BeanUtils.copyProperties(signUpForm, user);
 	    
-	 // 사용자 존재 여부 체크
-	    checkUser(user);
+	    // 비밀번호 암호화 후 설정
+	    user.setPassword(passwordEncoder.encode(signUpForm.getPassword()));
 
 	    // 사용자 저장
 	    userRepository.save(user);
